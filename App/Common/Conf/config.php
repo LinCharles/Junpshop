699f93e85b0da89720fcffe6b814a6afd4a75467
LinCharles <charleslim2013@gmail.com>
2014-05-01 21:23:40 +0800

    begin

---
+++ App/Common/Conf/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	// 关闭多模块访问
+	'MULTI_MODULE'          =>  false,
+	'DEFAULT_MODULE'        =>  'Home',
+	//URL地址不区分大小写,true为不区分，false为区分
+	'URL_CASE_INSENSITIVE'  =>  false,
+);
